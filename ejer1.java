167a3d7b3f453faaffa2f992ca9b66fac9860285
karinatabares1 <anakarinatabarescamargo707@gmail.com>
2024-09-04 23:59:42 -0500

    arreglos

---
+++ ejer1.java
@@ -0,0 +1,16 @@
+
+public class ejer1 {
+	public static void main(String[] args) {
+		  int[] numbers = {1, 2, 3, 4, 5};
+	        int sum = 0;
+
+	        for (int number : numbers) {
+	            sum += number;
+	        }
+
+	        System.out.println("Suma de los elementos del array: " + sum); 
+
+	}
+
+
+}
